fdf4a3ff9a3d14393f20c4d759204ff79a9c2b43
Ashish1414 <ashubansal.ashishbansal@gmail.com>
2020-02-03 21:58:15 +0530

    fragments created

---
+++ app/src/main/java/com/store/storekhata/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.store.storekhata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+import com.store.storekhata.onBoardingStructure.OnBoardingActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new CountDownTimer(3000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(SplashScreen.this , OnBoardingActivity.class);
+                startActivity(intent);
+            }
+        }.start();
+    }
+}
---
+++ app/src/main/java/com/store/storekhata/onBoardingStructure/OnBoardingActivity.java
@@ -0,0 +1,32 @@
+package com.store.storekhata.onBoardingStructure;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.store.storekhata.R;
+
+public class OnBoardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding);
+        Toolbar toolbar = findViewById(R.id.onBoardingToolbar);
+        setSupportActionBar(toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.on_boarding_fragment_container);
+        NavigationUI.setupActionBarWithNavController(this, navController);
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+}
